f02e292f3fa33f2105b64f4232809aefc1ce8ba6
reyali <79721654+reyali@users.noreply.github.com>
2021-02-27 21:16:45 +0700

    Add files via upload

---
+++ plugins/info.js
@@ -0,0 +1,31 @@
+let handler  = async (m, { conn, usedPrefix: _p }) => {
+  conn.reply(m.chat, `
+⚠️ *INFO BOT* ⚠️
+Dibuat dengan javascript via NodeJs,Ffmpeg,dan ImageMagick
+
+Rec: Sugar Dady
+Script: @Cedet
+
+*Thanks To :*
+Allah SWT :)
+
+║>Request? Wa.me/6282252420430
+╠═〘 INFO BOT 〙 ═
+`.trim(), m)
+}
+handler.help = ['info']
+handler.tags = ['info']
+handler.command = /^(info)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+module.exports = handler
+
